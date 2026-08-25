24a5a4ccb3a59b45a5224085f313607764dbd440
Amewu Emmanuel Mensah <emmanuel.amewu@dexwin.net>
2026-08-25 03:48:44 +0000

    Update todo.routes.ts

--- apps/api/src/module/todo.routes.ts
+++ apps/api/src/module/todo.routes.ts
@@ -32,8 +32,7 @@ export function toCreateTodoResponse(
 }
 
 type ListTodosResponse =
-  | { status: 200; body: TodoListResponse }
-  | { status: 500; body: InternalErrorResponse };
+  { status: 200; body: TodoListResponse } | { status: 500; body: InternalErrorResponse };
 
 export function toListTodosResponse(
   result: Result<Todo[], DatabaseError | ValidationError>,
