3606135141b122dac4af7f4b52a4e667a7ed55f0
Amewu Emmanuel Mensah <emmanuel.amewu@dexwin.net>
2026-09-22 08:05:21 +0000

    fix merge conflict for createTodo

--- apps/api/src/module/todo.service.ts
+++ apps/api/src/module/todo.service.ts
@@ -21,3 +21,40 @@ export function createTodoService(
 
   return createTodo(db, parsed.data);
 }
+<<<<<<< Updated upstream
+=======
+
+export function replaceTodoService(
+  db: Db,
+  id: string,
+  rawInput: unknown,
+): ResultAsync<Todo, RequestValidationError | NotFoundError | ValidationError | DatabaseError> {
+  const parsed = replaceTodoSchema.safeParse(rawInput);
+
+  if (!parsed.success) {
+    return errAsync({
+      type: "request_validation",
+      issues: parsed.error.issues.map((issue) => issue.message),
+    });
+  }
+
+  return replaceTodoById(db, id, parsed.data);
+}
+
+export function patchTodoService(
+  db: Db,
+  id: string,
+  rawInput: unknown,
+): ResultAsync<Todo, RequestValidationError | NotFoundError | ValidationError | DatabaseError> {
+  const parsed = patchTodoSchema.safeParse(rawInput);
+
+  if (!parsed.success) {
+    return errAsync({
+      type: "request_validation",
+      issues: parsed.error.issues.map((issue) => issue.message),
+    });
+  }
+
+  return patchTodoById(db, id, parsed.data);
+}
+>>>>>>> Stashed changes
